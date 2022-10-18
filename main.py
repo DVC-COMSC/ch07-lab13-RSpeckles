6c4cfda52e1a8cbc5b29d43d3f417a5a437f165f
RSpeckles <rchenspeckman@gmail.com>
2022-10-18 12:03:37 -0700

    Completed exercise

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 rnum = len(numbers)
 cnum = len(numbers[0])
+numCross = 0
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(len(numbers)):
+    for j in range(len(numbers[i])):
+        if numbers[i][j] == 1:
+            if i != len(numbers[i])-1 and j != len(numbers)-1:
+                if numbers[i-1][j] == 1 and numbers[i+1][j] == 1 and numbers[i][j-1] == 1 and numbers[i][j+1] == 1:
+                    numCross += 1
+                    print("(" + str(j+1) + ", " + str(i+1) + ")")
+
+print(numCross)
